f3fd1d3f71c98143697875b73ec06ae71e8fb589
MGD-Prog <PSMuz@mgdmail.ru>
2026-03-24 13:49:52 +0300

    add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,22 +31,22 @@ export default defineNuxtConfig({
         { name: 'creator', content: 'MGDArrow' },
         { name: 'apple-mobile-web-app-title', content: 'ОРОиК ГЕ' },
       ],
-      // link: [
-      //   {
-      //     rel: 'icon',
-      //     type: 'image/png',
-      //     href: '/favicon-96x96.png',
-      //     sizes: '96x96',
-      //   },
-      //   { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-      //   { rel: 'shortcut icon', href: '/favicon.ico' },
-      //   {
-      //     rel: 'apple-touch-icon',
-      //     href: '/apple-touch-icon.png',
-      //     sizes: '180x180',
-      //   },
-      //   { rel: 'manifest', href: '/site.webmanifest' },
-      // ],
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/favicon-96x96.png',
+          sizes: '96x96',
+        },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'shortcut icon', href: '/favicon.ico' },
+        {
+          rel: 'apple-touch-icon',
+          href: '/apple-touch-icon.png',
+          sizes: '180x180',
+        },
+        { rel: 'manifest', href: '/site.webmanifest' },
+      ],
     },
   },
 });
