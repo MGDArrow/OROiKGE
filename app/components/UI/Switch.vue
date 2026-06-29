38fb4e5fe910ec92a5048bf6feb69988b33216dc
MGDArrow <PSMuz@mgdmail.ru>
2026-06-29 21:39:46 +0300

    Добавлен новый компонент переключения между годами конкурса

---
+++ app/components/UI/Switch.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="switch">
+    <div
+      class="switch__point"
+      v-for="button in buttons"
+      :key="button"
+      @click="modelValue = button"
+      :class="{ active: button === modelValue }"
+    >
+      {{ button }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  interface Props {
+    buttons: string[];
+  }
+
+  const modelValue = defineModel<string>({ required: true });
+
+  const props = defineProps<Props>();
+</script>
+
+<style scoped lang="scss">
+  .switch {
+    display: flex;
+    gap: 20px;
+    width: 100%;
+    margin: 20px 0;
+    & div {
+      flex: 1;
+      padding: 10px;
+      font-weight: 600;
+      font-size: 1.5em;
+      text-align: center;
+      border: 4px solid var(--accent);
+      cursor: pointer;
+      transition: 0.3s ease-in-out;
+      &.active {
+        color: var(--color-white);
+        background: var(--accent);
+      }
+    }
+  }
+</style>
